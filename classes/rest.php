a34b34ab64883e4d2fb0da5c76b3ba7c5e4edaa8
Javier <00076017@uca.edu.sv>
2022-05-03 20:10:59 -0600

    version preliminar plugin para creacion de eventos en moodle

---
+++ classes/rest.php
@@ -0,0 +1,75 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Manage course custom fields
+ *
+ * @package local_googlecalendar
+ * @copyright 2022 Javier Mejia
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+ 
+namespace local_googlecalendar; 
+
+require_once($CFG->libdir . '/filelib.php');
+
+defined('MOODLE_INTERNAL') || die();
+
+class rest extends \core\oauth2\rest {                                                                                              
+                                                                                                                                    
+    /**                                                                                                                             
+     * Define the functions of the rest API.                                                                                        
+     *                                                                                                                              
+     * @return array Example:                                                                                                       
+     *  [ 'listFiles' => [ 'method' => 'get', 'endpoint' => 'http://...', 'args' => [ 'folder' => PARAM_STRING ] ] ]                
+     */                                                                                                                             
+    function get_api_functions() {                                                                                           
+        return [                                                                                                                    
+            'insert' => [                                                                                                           
+                'endpoint' => 'https://www.googleapis.com/calendar/v3/calendars/primary/events',                                                          
+                'method' => 'post',                                                                                                 
+                'args' => [                                                                                              
+                    'end' => PARAM_RAW,
+                    'start'  => PARAM_RAW,  
+                    'attendees' =>  PARAM_RAW,
+                    'summary' =>  PARAM_RAW,
+                    'eventType' => PARAM_RAW
+                                                                                                      
+                ],                                                                                                                  
+                'response' => 'xml'                                                                                                
+            ],
+            'create' => [                                                                                                           
+                'endpoint' => 'https://www.googleapis.com/calendar/v3/calendars',                                                          
+                'method' => 'post',                                                                                                 
+                'args' => [                                                                                              
+                    'summary' => PARAM_RAW,                                                            
+                ],                                                                                                                  
+                'response' => 'xml'                                                                                                
+            ] ,
+            'get' => [                                                                                                           
+                'endpoint' => 'https://www.googleapis.com/calendar/v3/calendars/primary',                                                          
+                'method' => 'get',                                                                                                 
+                'args' => [                                                                                              
+                    'summary' => PARAM_RAW,
+                                                                                                      
+                ],                                                                                                                  
+                'response' => 'xml'                                                                                                
+            ]                                                                                                                   
+        ];                                                                                                                          
+    }
+    
+}
---
+++ lib.php
@@ -0,0 +1,159 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     local_googlecalendar
+ * @copyright   2022 Javier Mejia
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * @param moodleform $formwrapper The moodle quickforms wrapper object.
+ * @param MoodleQuickForm $mform The actual form object (required to modify the form).
+ * https://docs.moodle.org/dev/Callbacks
+ * This function name depends on which plugin is implementing it. So if you were
+ * implementing mod_wordsquare
+ * This function would be called wordsquare_coursemodule_standard_elements
+ * (the mod is assumed for course activities)
+ */
+function local_googlecalendar_coursemodule_standard_elements($formwrapper, $mform) {
+    GLOBAL $DB;
+    $moduleid = $formwrapper->get_current()->coursemodule;
+    $courseid = $formwrapper->get_current()->course;
+    $modulename = $formwrapper->get_current()->modulename;
+    
+    $user = $DB->get_record_sql('SELECT checkbox FROM {googlecalendar} WHERE course = ? AND assign = ?;',[$courseid,$moduleid]);
+    
+    if ($modulename == 'assign') {
+        $elementname1 = 'checkboxGoogleCalendar';
+        $mform->addElement('header', 'exampleheader', get_string('message1', 'local_googlecalendar'));
+        $mform->addElement('advcheckbox', $elementname1, get_string('message1', 'local_googlecalendar'));
+        $mform->setType($elementname1, PARAM_BOOL);
+        if(empty($user)){
+            $mform->setdefault($elementname1, ['checkbydefault']);
+        }
+        else{
+            if($user->checkbox == 1){
+                $mform->setdefault($elementname1, 1);
+            }
+        }
+
+    }
+    
+}
+
+/**
+ * Process data from submitted form
+ *
+ * @param stdClass $data
+ * @param stdClass $course
+ * @return $data
+ */
+function local_googlecalendar_coursemodule_edit_post_actions($data, $course) {
+    GLOBAL $DB,$SESSION;
+    $modulename = $data->modulename;
+    if($modulename == 'assign'){
+        $context = context_course::instance($data->course);
+        //Find if the assign is already created
+        $user = $DB->get_record_sql('SELECT id FROM {googlecalendar} WHERE course = ? AND assign = ?;',[$data->course,$data->coursemodule]);
+        //Define Objects
+        $newobj = new stdClass();
+        $dateend = new stdClass();
+        $datestart = new stdClass();
+        //Obtain the course id
+        $newobj->course = $data->course;
+        //Obtain the assign id
+        $newobj->assign = $data->coursemodule;
+        //Obtain the value of the checkbox
+        $newobj->checkbox = $data->checkboxGoogleCalendar;
+        //Obtain the date when the activity start
+        $datestart->dateTime = date("Y-m-d",$data->allowsubmissionsfromdate) .'T'. date("H:m:s.000",$data->allowsubmissionsfromdate).'Z';
+        //Obtain the name of the assign
+        $summary = $data->name;
+        //Obtain the date when the activity end
+        $dateend->dateTime = date("Y-m-d",$data->duedate) .'T'. date("H:m:s.000",$data->duedate).'Z';
+        //Add variables of dateTime to add them in the database
+        $newobj->end = $dateend->dateTime;
+        $newobj->start = $datestart->dateTime;
+        //If the assign is already created just update, in other case insert the new assign
+        if(empty($user)){  
+            $DB->insert_record('googlecalendar',$newobj);
+        }
+        else{
+            $newobj->id = $user->id;
+            $DB->update_record('googlecalendar', $newobj);
+        }
+        //Check if the app need to send reminders and It's enable start and end date
+        if($newobj->checkbox == 1 and $newobj->end != '1970-01-01T01:01:00.000Z' and $newobj->start != '1970-01-01T01:01:00.000Z'){
+            //Get all users in the course
+            $submissioncandidates = get_enrolled_users($context, $withcapability = '', $groupid = 0, $userfields = 'u.*', $orderby = '', $limitfrom = 0, $limitnum = 0);
+            //Save each users email in array attendees 
+            $attendees = [];
+            foreach ($submissioncandidates as $d){
+                $attendee = new stdClass();
+                $attendee->email = $d->email;
+                array_push($attendees,$attendee);
+            } 
+            // Call API
+            // Get an issuer from the id
+            $issuer = \core\oauth2\api::get_issuer(1);
+            // Put in the returnurl the course id and sesskey
+            $sesskey = sesskey();   
+            $params = array('id' => $data->course, 'sesskey' => $sesskey);
+            // Get an OAuth client from the issuer
+            $returnurl  = new moodle_url('/course/view.php',$params);
+            // Add all scopes for the API
+            $scopes = 'https://www.googleapis.com/auth/calendar';
+            $client = \core\oauth2\api::get_user_oauth_client($issuer, $returnurl , $scopes);
+            $eventType = 'outOfOffice';
+            // Check the google session
+            if (!$client->is_logged_in()) {
+                redirect($client->get_login_url());
+            }
+            else{   
+                $service = new \local_googlecalendar\rest($client);
+                $params = [
+                    'end' => $dateend,
+                    'summary' => $summary,
+                    'start' => $datestart,
+                    'attendees' => $attendees,
+                    'eventType' => $eventType
+                ];      
+                $SESSION->myvar = $params;
+                $service->call('insert',[],json_encode($SESSION->myvar));
+            }
+        }  
+    } 
+    return $data;
+}
+
+
+/**
+ * Validate the data in the new field when the form is submitted
+ *
+ * @param moodleform_mod $fromform
+ * @param array $fields
+ * @return void
+ */
+function local_googlecalendar_coursemodule_validation($fromform, $fields) {
+    if (get_class($fromform) == 'mod_assign_mod_form') {
+       // \core\notification::add($fields[1], \core\notification::INFO);
+    }
+}
